8b9ef813d41987eec2496996646a32e79b702a96
fsanchez-originsoftware <fsanchez@originsoftware.com.ar>
2015-08-05 14:46:03 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+CouchbaseItemsStats
+===================
+
+Reads a Couchbase's bucket metadata and generates a report with statistics about stored items.
+The keys are grouped using a regex and for each group calculates:
+* count
+* average expiration time
+* stardard deviation of expiration time
+
+## Usage
+
+1. Add a view in the bucket with the following code:
+	* Go to Couchbase Console (Web UI) > `Views` > `Create Development View`
+	* Enter a `Design Document Name` and `View Name`
+	* Enter the following code:
+	```js
+    function (doc, meta) {
+      emit(meta.id, meta);
+    }
+    ```
+    * Save
+2. Edit settings in config file
+	* `<couchbase>`: Bucket connection.
+	* `viewDesignName`: Design document/view name to consume.
+    * `viewName`: View name to consume.
+    * `keyPrefixRegex`: A regex used to extract the prefix from each key. It's used in grouping function.
+    * `reportFormat`: Format used to store in the report. Only `json` is suppported by now.
+    * `reportFilePath`: Path use to store the report file.
+3. Run the .exe and the report file will be generated
---
+++ src/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Couchbase;
+using Newtonsoft.Json;
+
+namespace CouchbaseItemsStats
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Connecting with Couchbase...");
+                var client = new CouchbaseClient();
+
+                Console.WriteLine("Obtaining items metadata...");
+                var metadata = client.GetView(ConfigurationManager.AppSettings["viewDesignName"], ConfigurationManager.AppSettings["viewName"]);
+
+                Console.WriteLine("Analyzing data...");
+                var stats = metadata
+                    .GroupBy(x => GetKeyPrefix(x.ItemId),
+                        elementSelector: x => (long)((IDictionary<string, object>)x.Info["value"])["expiration"])
+                    .Select(x => new StatItem()
+                    {
+                        keyPrefix = x.Key,
+                        count = x.Count(),
+                        expirationAvg = x.Average(),
+                        expirationStdDev = GetStdDev(x)
+                    });
+
+                Console.WriteLine("Storing...");
+                var data = Serialize(stats);
+                File.WriteAllText(ConfigurationManager.AppSettings["reportFilePath"], data);
+
+                Console.WriteLine("Done!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.Write("Press enter to exit...");
+                Console.ReadLine();
+            }
+        }
+
+        private static string GetKeyPrefix(string key)
+        {
+            return Regex.Match(key, ConfigurationManager.AppSettings["keyPrefixRegex"]).Value;
+        }
+
+        private static double GetStdDev(IEnumerable<long> values)
+        {
+            double average = values.Average();
+            double sumOfSquaresOfDifferences = values.Select(val => (val - average) * (val - average)).Sum();
+            return Math.Sqrt(sumOfSquaresOfDifferences / values.Count());
+        }
+
+        private static string Serialize(IEnumerable<StatItem> stats)
+        {
+            var format = ConfigurationManager.AppSettings["reportFormat"];
+            if (format == "json")
+                return JsonConvert.SerializeObject(stats, Formatting.Indented);
+
+            throw new NotImplementedException(format + " is not an implemented report format");
+        }
+    }
+
+    internal class StatItem
+    {
+        public string keyPrefix { get; set; }
+        public long count { get; set; }
+        public double expirationAvg { get; set; }
+        public double expirationStdDev { get; set; }
+    }
+}
