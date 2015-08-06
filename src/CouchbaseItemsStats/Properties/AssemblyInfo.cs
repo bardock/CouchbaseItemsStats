b36ff818928db52ff19794d9b26c9347a0ff6a2a
bardock <fedesanchez89@gmail.com>
2015-08-06 17:32:52 -0300

    v.0.1.0

--- src/CouchbaseItemsStats/Properties/AssemblyInfo.cs
+++ src/CouchbaseItemsStats/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CouchbaseItemsStats")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Reads a Couchbase's bucket metadata and generates a report with statistics about stored items")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("bardock")]
 [assembly: AssemblyProduct("CouchbaseItemsStats")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
